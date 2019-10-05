ef292523ed9a7cee03b1c0e70586ee4860f475a2
Manel <mmpcp23@gmail.com>
2019-10-05 14:57:56 +0100

    Obstacle Avoid - Done | Character Avoid - WIP

---
+++ Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicAvoidCharacter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Assets.Scripts.IAJ.Unity.Util;
+using UnityEngine;
+
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+    public class DynamicAvoidCharacter : DynamicMovement
+    {
+        public float MaxTimeLookAhead { get; set; }
+        public float AvoidMargin { get; set; }
+
+        private KinematicData OtherCharacter;
+
+        public override string Name
+        {
+            get { return "Avoid Character"; }
+        }
+
+        public DynamicAvoidCharacter(KinematicData other)
+        {
+            this.Output = new MovementOutput();
+            this.OtherCharacter = other;
+        }
+
+        public override MovementOutput GetMovement()
+        {
+            var DeltaPos = this.OtherCharacter.Position - this.Character.Position;
+            var DeltaVel = this.OtherCharacter.velocity - this.Character.velocity;
+
+            var DeltaSqrSpeed = DeltaVel.sqrMagnitude;
+
+            if (DeltaSqrSpeed == 0) return this.Output;
+
+            var TimeToClosest = -Vector3.Dot(DeltaPos, DeltaVel) / DeltaSqrSpeed;
+
+            if (TimeToClosest > MaxTimeLookAhead) return this.Output;
+
+            var FutureDeltaPos = DeltaPos + DeltaVel * TimeToClosest;
+            var FutureDistance = FutureDeltaPos.magnitude;
+
+            if (FutureDistance > 2 * AvoidMargin) return this.Output;
+
+            if (FutureDistance <= 0 || DeltaPos.magnitude < 2 * AvoidMargin)
+            {
+                this.Output.linear = this.Character.Position - this.OtherCharacter.Position;
+            } 
+            else 
+            {
+                this.Output.linear = FutureDeltaPos * -1;
+            }
+
+            this.Output.linear = this.Output.linear.normalized * this.MaxAcceleration;
+
+            return Output;
+        }
+    }
+}
---
+++ Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicAvoidObstacle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Assets.Scripts.IAJ.Unity.Util;
+using UnityEngine;
+
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+    public class DynamicAvoidObstacle : DynamicSeek
+    {
+        public float AvoidMargin { get; set; }
+        public float MaxLookAhead { get; set; }
+
+        private RaycastHit hit;
+        private Collider CollisionDetector;
+
+        public DynamicAvoidObstacle(GameObject obstacle)
+        {
+            this.CollisionDetector = obstacle.GetComponent<Collider>();
+            base.Target = new KinematicData();
+        }
+
+
+        public override MovementOutput GetMovement()
+        {
+            if (base.Character.velocity.magnitude > 0)
+            {
+                Ray[] rays = new Ray[3];
+                float[] lookAheads = new float[3];
+
+                // Left whisker
+                Vector3 newdirection = MathHelper.Rotate2D(base.Character.velocity, MathConstants.MATH_1_PI).normalized;
+                rays[0] = new Ray(base.Character.Position, newdirection);
+                lookAheads[0] = this.MaxLookAhead / 2;
+
+                // Right whisker
+                newdirection = MathHelper.Rotate2D(base.Character.velocity, -MathConstants.MATH_1_PI).normalized;
+                rays[1] = new Ray(base.Character.Position, newdirection);
+                lookAheads[1] = this.MaxLookAhead / 2;
+
+                // centralRay
+                rays[2] = new Ray(base.Character.Position, base.Character.velocity.normalized);
+                lookAheads[2] = this.MaxLookAhead;
+
+                bool collision;
+                for (int i = 0; i < 3; i++)
+                {
+                    collision = this.CollisionDetector.Raycast(rays[i], out hit, lookAheads[i]);
+
+                    if (collision)
+                    {
+                        base.Target.Position = hit.point + hit.normal * this.AvoidMargin;
+
+                        return base.GetMovement();
+                    }
+                }
+
+            }
+
+            return new MovementOutput();
+        }
+    }
+}
